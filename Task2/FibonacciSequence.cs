52b660a79644a9039a614ff48aebdec32f593878
zzhukanton <zzhukanton@gmail.com>
2015-11-04 13:49:12 +0200

    Task2 complete

---
+++ Task2/FibonacciSequence.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public static class FibonacciSequence
+    {
+        public static IEnumerable<int> GetSequence(int number)
+        {
+            if (number < 1)
+                throw new ArgumentException("Number is less than 1");
+
+            yield return 0;
+
+            if (number == 1)
+                yield break;
+            yield return 1;
+
+            if (number == 2)
+                yield break;
+
+            int first = 0, second = 1;
+            for (int i = 0; i < number; i++)
+            {
+                int temp = first + second;
+                first = second;
+                second = temp;
+                yield return second;
+            }
+
+            yield break;
+        }
+    }
+}
