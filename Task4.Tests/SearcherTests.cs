9a65393e384e4eed4b3c678a30289ab3a41faffa
zzhukanton <zzhukanton@gmail.com>
2015-11-04 13:26:42 +0200

    Task4 complete

---
+++ Task4.Tests/SearcherTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Task4;
+
+namespace Task4.Tests
+{
+    [TestFixture]
+    public class SearcherTests
+    {
+        [TestCase(new int[] { 1, 4, 6, 19, 20 }, 4, Result = 1)]
+        [TestCase(new int[] { 1, 4, 6, 19, 20 }, 0, Result = -1)]
+        [TestCase(null, 1, ExpectedException = typeof(ArgumentNullException))]
+        public int BinarySeach_DefaultWithInts(int[] array, int element)
+        {
+            return array.BinarySearch<int>(element);
+        }
+
+        [TestCase(new int[] { 1, 4, 6, 19, 20 }, 4, null, ExpectedException = typeof(ArgumentNullException))]
+        public int BinarySearch_WithComparerWithInts(int[] array, int element, Comparer<int> comparer)
+        {
+            return array.BinarySearch<int>(element, comparer);
+        }
+
+        [TestCase(new int[] { 1, 4, 6, 19, 20 }, 4, null, ExpectedException = typeof(ArgumentNullException))]
+        public int BinarySearch_WithComparisionWithInts(int[] array, int element, Comparison<int> comparision)
+        {
+            return array.BinarySearch<int>(element, comparision);
+        }
+
+        [TestCase(new char[] { 'a', 'b', 'd', 'f', 'j' }, 'f', Result = 3)]
+        [TestCase(new char[] { 'a', 'b', 'd', 'f', 'j' }, 'k', Result = -1)]
+        [TestCase(null, 'f', ExpectedException = typeof(ArgumentNullException))]
+        public int BinarySearch_WithCharsDefault(char[] array, char element)
+        {
+            return array.BinarySearch<char>(element);
+        }
+    }
+}
---
+++ Task4/Searcher.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    public static class Searcher
+    {
+        public static int BinarySearch<T>(this T[] array, T element, IComparer<T> comparer)
+        {
+            if (array == null)
+                throw new ArgumentNullException("Array is null");
+            if (comparer == null)
+                throw new ArgumentNullException("Comparer is null");
+
+            int middle, left = 0, right = array.Length - 1;
+
+            while (left <= right)
+            {
+                middle = left + (right - left) / 2;
+
+                int order = comparer.Compare(array[middle], element);
+                if (order == 0)
+                    return middle;
+                if (order > 0)
+                    right = middle - 1;
+                else
+                    left = middle + 1;
+            }
+            return -1;
+        }
+
+        public static int BinarySearch<T>(this T[] array, T element)
+        {
+            if (array == null)
+                throw new ArgumentNullException("Array is null");
+
+            return array.BinarySearch<T>(element, Comparer<T>.Default);
+        }
+
+        public static int BinarySearch<T>(this T[] array, T element, Comparison<T> comparision)
+        {
+            if (comparision == null)
+                throw new ArgumentNullException("Comparision is null");
+
+            return array.BinarySearch<T>(element, Comparer<T>.Create(comparision));
+        }
+    }
+}
